e34ed1387b2f873bf622ad7d1700e640cc1a411b
DICSA <belovrl1@gmail.com>
2022-03-25 00:05:21 +0300

    Добавил UIDatePicker, UiScrollView и UISwitch c текстом

--- UISegmentedControl/UISegmentedControl/ViewController.swift
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var switchLabel: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +34,8 @@ class ViewController: UIViewController {
         slider.minimumTrackTintColor = .orange
         slider.maximumTrackTintColor = .black
         slider.thumbTintColor = .gray
+        
+        datePicker.locale = Locale(identifier: "ru_RU")
         
         
         
@@ -77,8 +83,29 @@ class ViewController: UIViewController {
             label.text = textField.text
             textField.text = nil
         }
+    }
+    @IBAction func changeDate(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        let dateValue = dateFormatter.string(from: sender.date)
+        label.text = dateValue
+    }
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+         
+        segmentedControl.isHidden = !segmentedControl.isHidden //!segmentedControl.isHidden - ! ставит значение на противоположенное
+        label.isHidden = !label.isHidden
+        slider.isHidden = !slider.isHidden
+        textField.isHidden = !textField.isHidden
+        doneButton.isHidden = !doneButton.isHidden
+        datePicker.isHidden = !datePicker.isHidden
+    
         
-
+        if sender.isOn {
+            switchLabel.text = "Отобразить все элементы"
+        } else {
+            switchLabel.text = "Скрыть все элементы"
+        }
     }
 }
 
